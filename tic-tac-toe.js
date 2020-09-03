bdfd2f75bfc5fad8caa772a7dad224334d054a78
Chris Resnick <chris.resnick@gmail.com>
2020-09-03 18:38:41 -0400

    done

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,6 +7,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     let newGameButton = document.getElementById("new-game");
     newGameButton.disabled = true;
     let resetButton = document.getElementById("reset-button");
+    repopulate();
 
 
     board.addEventListener("click", (e) => {
@@ -18,19 +19,46 @@ window.addEventListener("DOMContentLoaded", (event) => {
                 // e.target.innerHtml = "<img src=https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg>"
                 e.target.style.backgroundImage =
                 "url(https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg)";
-                // saveStatus();
+                saveStatus();
             checkForWinner(x, 'X');
             } else {
                 o.push(e.target.className);
                 e.target.style.backgroundImage =
                 "url(https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg)"
-                // saveStatus();
+                saveStatus();
                 checkForWinner(o, 'O');
             }
             turn = !turn;
         }
 
     });
+
+    function saveStatus(){
+        let divs = document.querySelectorAll('div');
+        divs.forEach(div => {
+            if (div.className.includes("row")) {
+                let bgImage = div.style.backgroundImage;
+                localStorage.setItem(div.id, bgImage);
+            }
+        })
+    }
+
+    function repopulate(){
+        let divs = document.querySelectorAll('div');
+        divs.forEach(div => {
+            if (div.className.includes("row")) {
+                div.style.backgroundImage = localStorage.getItem(div.id);
+                if (div.style.backgroundImage.includes('player-x')){
+                    x.push(div.className);
+                    checkForWinner(x, "X");
+                }
+                else if (div.style.backgroundImage.includes("player-o")){
+                    o.push(div.className);
+                    checkForWinner(o, "O");
+                }
+            }
+        });
+    }
 
     newGameButton.addEventListener("click", e => {
         let divs = document.querySelectorAll('div');
@@ -68,7 +96,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
                 return;
             }
         }
-        //Checks for Edge Case of Diagonal 
+        //Checks for Edge Case of Diagonal
         if(arr.includes("square row-2 col-2")){
             if(arr.includes("square row-1 col-1") && arr.includes("square row-3 col-3")){
                 winner.innerHTML = `Winner: ${team}`;
